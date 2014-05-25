1cd501f26945efcf0c8becbd33f11e26886c6ffc
mjuneja16 <mohit.juneja16@gmail.com>
2014-05-25 21:39:34 +0530

    commit all changes

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,50 @@
-## Put comments here that give an overall description of what your
-## functions do
+##'cacheSolve' function calculates the inverse of the special "matrix" created with the 'makeCacheMatrix' function.
 
-## Write a short comment describing this function
+##'makeCacheMatrix' function creates a special "matrix",
+##which is really a list containing a function to
+##set the value of the matrix using the 'set' function,
+##get the value of the matrix using the 'get' function,
+##set the value of the inverse matrix using the 'setInverse' function and
+##get the value of the inverse matrix using the 'getInverse' function.
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix())
+{               
+        s<-NULL
+	set <- function(y)
+	{
+		x <<- y
+		s <<- NULL
+	}
+	get <- function() x
+	setInverse <- function(solve) s<<-solve
+	getInverse <- function() s
+	list(set = set, get = get, 
+	     setInverse = setInverse,
+	     getInverse = getInverse)
 }
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+##'cacheSolve' function calculates the inverse of the special "matrix" 
+##created with the 'makeCacheMatrix' function.
+##At first, it checks the determinant of the matrix.
+##valueDet is an numeric vector which indicates the determinant of the matrix.
+##If determinant is zero, then it displays the message that the determinant is zero.
+##If determinant is non-zero, it checks if the inverse has already been calculated. 
+##in this case, it gets the inverse from the cache and skips the computation.
+##Otherwise,it calculates the inverse of the cached data and 
+##sets the value of the inverse in the cache via the setInverse function.
+cacheSolve <- function(x, ...) 
+{
+        s <- x$getInverse()
+	if(!is.null(s))
+	{
+		message("getting cached data")
+		return(s)
+	}
+	data <- x$get()								        valueDet<-det(data)
+	if(valueDet==0)									{
+		message("Determinant = 0 .i.e. It is a non-singular matrix.\nThus, Inverse of the Matrix cannot be computed.")
+	}
+	else
+	{
+		s<- solve(data, ...)								x$setInverse(s)}
+		s									}
 }
